07b71e0109fcb6b188de1347da2ef0cb28dd2886
Amit Kumar <amitkumar655921@gmail.com>
2024-02-29 15:30:19 +0530

    serverSetup():done

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connection = require("./db");
+
+const port = process.env.PORT;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the server" });
+});
+
+app.listen(port, async () => {
+  try {
+    await connection;
+    console.log(`Server is running on port: ${port}`);
+  } catch (error) {
+    console.log(`Error while starting the server: ${error}`);
+  }
+});
